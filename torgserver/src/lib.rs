7931d7082370db56a70464c18fbf0a877f734621
Will <wnonnemaker@gmail.com>
2025-03-18 12:59:19 -0700

    passes unit tests for user storage

--- torgserver/src/lib.rs
+++ torgserver/src/lib.rs
@@ -3,6 +3,7 @@ use actix_web::{HttpRequest, HttpResponse, Responder, post, web};
 use serde::{Serialize, Deserialize};
 use serde_json;
 use std::collections::HashMap;
+use std::sync::RwLock;
 
 #[derive(Serialize, Deserialize)]
 struct SignUpRequest {
@@ -24,7 +25,7 @@ impl SignUpRequest {
             password: self.password,
         }
     }
-}
+} 
 
 #[derive(Debug)]
 struct User {
@@ -45,15 +46,21 @@ impl UserStorage {
     }
 
     pub fn add_user(&mut self, user: User) -> Result<(), &'static str> {
-        match self.users.insert(user.email.to_string(), Box::new(user)) {
+        let map = &mut self.users; 
+        match map.insert(user.email.to_string(), Box::new(user)) {
             None => Ok(()),
             Some(_) => Err("User already exists"),
         }
     }
 
-    pub fn login(&mut self, email: String) -> Result<&User, &'static str> {
-        if let Some(user) = self.users.get(&email) {
-            Ok(user.as_ref())
+    pub fn login(&self, email: String) -> Result<User, &'static str> {
+        let map = &self.users;
+        if let Some(user) = map.get(&email) {
+            let retuser = User {
+                email: user.email.clone(),
+                password: user.password.clone(),
+            };
+            Ok(retuser)
         } else {
             Err("No account under that email exists")
         }
@@ -66,15 +73,22 @@ pub async fn fallback(_req: HttpRequest) -> impl Responder {
 }
 
 #[post("/signup")]
-pub async fn signup(req: web::Json<SignUpRequest>) -> impl Responder {
+pub async fn signup(req: web::Json<SignUpRequest>, data: web::Data<UsersLock>) -> impl Responder {
     if req.email.is_empty() || req.password.is_empty() {
         return HttpResponse::BadRequest().body("All fields must have a value");
     }
+    let mut users = data.userbox.write().unwrap();
+    (*users).add_user(User {email: req.email.clone(), password: req.password.clone()});
     HttpResponse::Created().json(serde_json::json!({
-        "message" : "User regustered successfully!",
+        "message" : "User registered successfully!",
         "email" :  req.email
     }))
 }
+
+struct UsersLock {
+    userbox: RwLock<UserStorage>,
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -84,7 +98,9 @@ mod tests {
 
     #[actix_web::test]
     async fn test_index_post() {
+        let data = web::Data::new( UsersLock { userbox: RwLock::new(UserStorage::new()) });
         let app = test::init_service(App::new()
+            .app_data(data.clone())
             .service(signup))
             .await;
         let newUser = SignUpRequest::new("torg@torg.net", "apples");
@@ -93,13 +109,14 @@ mod tests {
           .set_json(newUser)
           .to_request();
         let resp = test::call_service(&app, req).await;
-        println!("Response Statis: {:?}", resp.status());
         assert!(resp.status().is_success());
     }
 
     #[actix_web::test]
-    async fn test_index_post() {
+    async fn test_signup_userhash() {
+        let data = web::Data::new( UsersLock { userbox: RwLock::new(UserStorage::new()) });
         let app = test::init_service(App::new()
+            .app_data(data.clone())
             .service(signup))
             .await;
         let newUser = SignUpRequest::new("torg@torg.net", "apples");
@@ -110,10 +127,15 @@ mod tests {
         let resp = test::call_service(&app, req).await;
         println!("Response Statis: {:?}", resp.status());
         assert!(resp.status().is_success());
+
+        let mut userstor = data.userbox.read().unwrap();
+
+        let result = (*userstor).login("torg@torg.net".to_string());
+        assert!(result.is_ok());
     }
 
     #[test]
-    async fn create_user_twice() {
+    async fn test_create_user_twice() {
         let mut storage = UserStorage::new();
 
         let user1 = User {
@@ -145,7 +167,7 @@ mod tests {
         let result = storage.add_user(user1);
 
         let login_result = storage.login("geek@geek.com".to_string());
-        assert!(result.is_ok());
+        assert!(login_result.is_ok());
 
         let login_result_fail = storage.login("boomer@cabin.com".to_string());
         assert!(login_result_fail.is_err());
