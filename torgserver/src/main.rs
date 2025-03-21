2766450c712b3cd166a016c715db0041df775d86
Will <wnonnemaker@gmail.com>
2025-03-20 19:47:51 -0700

    adding service functions for main.rs

--- torgserver/src/main.rs
+++ torgserver/src/main.rs
@@ -6,6 +6,35 @@ mod models;
 
 use models::*;
 
+#[post("/signup")]
+pub async fn signup(req: web::Json<SignUpRequest>, data: web::Data<UsersLock>) -> impl Responder {
+    if !req.is_valid() {
+        return HttpResponse::BadRequest().body("All fields must have a value");
+    }
+    let mut users = data.userbox.write().unwrap();
+    let email = req.get_email();
+    (*users).add_user(req.req_to_user());
+    HttpResponse::Created().json(serde_json::json!({
+        "message" : "User registered successfully!",
+        "email" :  email,
+    }))
+}
+
+//need to validate login and give session token back to client
+#[post("/login")]
+pub async fn login(req: web::Json<LoginRequest>, data: web::Data::<UsersLock>) -> impl Responder {
+    if !req.is_valid() {
+        return HttpResponse::BadRequest().body("All fields must have a value");
+    }
+    let users = data.userbox.read().unwrap();
+    if let Ok(login_result) = (*users).login(req.req_to_user()) {
+        return HttpResponse::Accepted().body("Login success");
+    } else {
+        return HttpResponse::Forbidden().body("Incorrect Password");
+    }
+
+}
+
 #[actix_web::main]
     async fn main() -> std::io::Result<()> {
         let data = web::Data::new( UsersLock::new() );
